187128c438793ec36ce79b72ca31759ef15cbfc0
shinded22 <dhananjay.shinde@rotary.org>
2024-07-18 18:08:27 +0000

    terraform and Azure and windows VMs

---
+++ azure-windows/outputs.tf
@@ -0,0 +1,15 @@
+output "public_ip_address" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "test1_public_ip_address" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "test2_public_ip_address" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "test3_public_ip_address" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
